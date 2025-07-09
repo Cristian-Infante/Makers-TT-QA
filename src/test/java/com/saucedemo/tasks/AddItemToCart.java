5399845b783457edd5597dfc4a00a4f84b318a54
Cristian Infante <desarrollo.software@alullanos.com>
2025-07-09 00:08:42 -0500

    Fix: Migration from WebDriver to Playwright

--- src/test/java/com/saucedemo/tasks/AddItemToCart.java
+++ src/test/java/com/saucedemo/tasks/AddItemToCart.java
@@ -3,9 +3,12 @@ package com.saucedemo.tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.playwright.Target;
 import net.serenitybdd.screenplay.playwright.interactions.Click;
 
 public class AddItemToCart implements Task {
+
+    private static final String ADD_TO_CART_TEMPLATE = "//div[text()='%s']/ancestor::div[@class='inventory_item']//button";
 
     private final String productName;
 
@@ -16,13 +19,12 @@ public class AddItemToCart implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
 
-        String locator = String.format(
-                "//div[text()='%s']/ancestor::div[@class='inventory_item']//button",
-                productName
-        );
+        Target addToCartButton = Target.the(
+                        String.format("add-to-cart button for product %s", productName))
+                .locatedBy(String.format(ADD_TO_CART_TEMPLATE, productName));
 
         actor.attemptsTo(
-                Click.on(locator)
+                Click.on(addToCartButton)
         );
     }
 
